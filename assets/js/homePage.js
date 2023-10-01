ed5a86e4bb0b21957085a9ce4f732372f297560c
karimliaghabala <agabala.93@gmail.com>
2023-10-01 19:04:54 +0400

    update response

--- assets/js/homePage.js
+++ assets/js/homePage.js
@@ -1,30 +1,40 @@
-
-/*<<<<<<<<<<<<<<<<<<<<<<navbar section start>>>>>>>>>>>>>>>>>*/ 
+/*<<<<<<<<<<<<<<<<<<<<<<navbar section start>>>>>>>>>>>>>>>>>*/
 let icon1 = document.querySelector(".icon-lang");
 let icon2 = document.querySelector(".icon-menu");
+let icon3 = document.querySelector("#icon-exit");
 let menu = document.querySelector(".response-menu");
 let languages = document.querySelector(".lang");
+
 let lang = false;
+let resMenu = false;
+let closemenu = false;
+//langugage active and deactive
+icon1.addEventListener("click", () => {
+  if (!lang) {
+    languages.style.display = "block";
+  } else {
+    languages.style.display = "none";
+  }
+  lang = !lang
+});
+//response menu active
+icon2.addEventListener("click", () => {
+  if (!resMenu) {
+    menu.style.transform = "translateX(0)";
+  }
+  else{
+    menu.style.transform = "translateX(120%)"
+  }
+  resMenu = !resMenu
+});
+//response menu deactive
+icon3.addEventListener("click", () => {
+  if (!closemenu) {
+    menu.style.transform = "translateX(120%)"
+  }  else{
+    menu.style.transform = "translateX(0)"
+  }
+  closemenu = !closemenu
+});
 
-icon1.addEventListener('click',() => {
-    if(!lang){
-        languages.style.display = "block";
-        lang = true;
-    }
-    else {
-        languages.style.display = "none";
-        lang = false;
-    }
-})
-
-icon2.addEventListener('click',()=>{
-    if(!lang){
-        menu.style.transform = "translateX(0)";
-        lang = true;
-    }else {
-        menu.style.display = "translateX(120%)";
-        lang = false;
-    }
-
-})
-/*<<<<<<<<<<<<<<<<<<<<<<navbar section ended>>>>>>>>>>>>>>>>>*/ 
+/*<<<<<<<<<<<<<<<<<<<<<<navbar section ended>>>>>>>>>>>>>>>>>*/
